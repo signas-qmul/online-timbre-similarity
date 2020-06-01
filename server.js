6cff0115fc229357610faadb2a962de242f059bc
Ben Hayes <ben@benhayes.net>
2020-06-01 10:43:25 +0100

    Test linting

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static('client'));
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test', function(err, client) {
   if (err) {
-    console.log(err);
+       console.log(err);
     process.exit(1);
   }
 
